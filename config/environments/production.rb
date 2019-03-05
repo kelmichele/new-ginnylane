1db0b95c10ea9de5c0f98ee22a7123cd15f8d45c
kelmichele <kelli@hsdesignhouse.com>
2019-03-05 17:01:43 -0600

    readded production info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,22 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'hsdesignhouse.com',
+    user_name:            Rails.application.credentials.gmail[:g_user],
+    password:             Rails.application.credentials.gmail[:g_pass],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+  
+  config.action_mailer.default_url_options = {
+  host: "hsdesignhouse.com"
+  }
+
+  config.action_mailer.default_options = {
+  from: "noreply@ginnylanebargrill.com"
+  }
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
