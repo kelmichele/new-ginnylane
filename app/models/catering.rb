5e2db8703845ac7543dd902820292d5627b10db6
kelmichele <kelli@hsdesignhouse.com>
2025-04-12 08:42:54 -0500

    rmv og unused catering set up

--- app/models/catering.rb
+++
@@ -1,8 +0,0 @@
-class Catering < ApplicationRecord
-  validates :name, presence: true
-  validates :phone, presence: true
-  validates :email, presence: true
-  validates :date, presence: true
-  validates :guest, presence: true
-  validates :content, presence: true
-end
